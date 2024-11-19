6c58af21cab7a2e18584a6edecbc95b6cebb8d6d
Sarocha <rocha.right007@gmail.com>
2024-11-19 18:18:28 +0700

    edit job title

--- html/images/script.js
+++ html/images/script.js
@@ -26,6 +26,12 @@ let projectsLink = document.getElementById("goToProjects");
 		e.preventDefault();
 	});
 
+	let viewProjectList = document.getElementById('ProjectList');
+	viewProjectList.addEventListener('click',function(e){
+		displayProjectsList()
+		e.preventDefault;
+	});
+
 	let viewProject1 = document.getElementById('viewProject1');
 	viewProject1.addEventListener('click',function(e){
 		displayProject1();
@@ -43,36 +49,127 @@ let projectsLink = document.getElementById("goToProjects");
 		displayProject3();
 		e.preventDefault;
 	});
+
+	let viewProject4 = document.getElementById('viewProject4');
+	viewProject4.addEventListener('click',function(e){
+		displayProject4();
+		e.preventDefault;
+	});
+
+	let viewProject5 = document.getElementById('viewProject5');
+	viewProject5.addEventListener('click',function(e){
+		displayProject5();
+		e.preventDefault;
+	});
+}
+
+function displayProjectsList(){
+	let allProjects = document.querySelector('.allProjects');
+	let project1 = document.querySelector('.project1');
+	let project2 = document.querySelector('.project2');
+	let project3 = document.querySelector('.project3');
+	let project4 = document.querySelector('.project4');
+	let project5 = document.querySelector('.project5');
+	let projectListButton = document.querySelector('.projectList');
+
+	allProjects.style.display = "block";
+	project1.style.display = "none";
+	project2.style.display = "none";
+	project3.style.display = "none";
+	project4.style.display = "none";
+	project5.style.display = "none";
+	projectListButton.style.display = "none";
 }
 
 function displayProject1(){
+	let allProjects = document.querySelector('.allProjects');
 	let project1 = document.querySelector('.project1');
 	let project2 = document.querySelector('.project2');
 	let project3 = document.querySelector('.project3');
+	let project4 = document.querySelector('.project4');
+	let project5 = document.querySelector('.project5');
+	let projectListButton = document.querySelector('.projectList');
 
+	allProjects.style.display = "none";
 	project1.style.display = "block";
 	project2.style.display = "none";
 	project3.style.display = "none";
+	project4.style.display = "none";
+	project5.style.display = "none";
+	projectListButton.style.display = "block";
 }
 
 function displayProject2(){
+	let allProjects = document.querySelector('.allProjects');
 	let project1 = document.querySelector('.project1');
 	let project2 = document.querySelector('.project2');
 	let project3 = document.querySelector('.project3');
+	let project4 = document.querySelector('.project4');
+	let project5 = document.querySelector('.project5');
+	let projectListButton = document.querySelector('.projectList');
 
+
+	allProjects.style.display = "none";
 	project1.style.display = "none";
 	project2.style.display = "block";
 	project3.style.display = "none";
+	project4.style.display = "none";
+	project5.style.display = "none";
+	projectListButton.style.display = "block";
 }
 
 function displayProject3(){
+	let allProjects = document.querySelector('.allProjects');
 	let project1 = document.querySelector('.project1');
 	let project2 = document.querySelector('.project2');
 	let project3 = document.querySelector('.project3');
+	let project4 = document.querySelector('.project4');
+	let project5 = document.querySelector('.project5');
+	let projectListButton = document.querySelector('.projectList');
 
+	allProjects.style.display = "none";
 	project1.style.display = "none";
 	project2.style.display = "none";
 	project3.style.display = "block";
+	project4.style.display = "none";
+	project5.style.display = "none";
+	projectListButton.style.display = "block";
+}
+
+function displayProject4(){
+	let allProjects = document.querySelector('.allProjects');
+	let project1 = document.querySelector('.project1');
+	let project2 = document.querySelector('.project2');
+	let project3 = document.querySelector('.project3');
+	let project4 = document.querySelector('.project4');
+	let project5 = document.querySelector('.project5');
+	let projectListButton = document.querySelector('.projectList');
+
+	allProjects.style.display = "none";
+	project1.style.display = "none";
+	project2.style.display = "none";
+	project3.style.display = "none";
+	project4.style.display = "block";
+	project5.style.display = "none";
+	projectListButton.style.display = "block";
+}
+
+function displayProject5(){
+	let allProjects = document.querySelector('.allProjects');
+	let project1 = document.querySelector('.project1');
+	let project2 = document.querySelector('.project2');
+	let project3 = document.querySelector('.project3');
+	let project4 = document.querySelector('.project4');
+	let project5 = document.querySelector('.project5');
+	let projectListButton = document.querySelector('.projectList');
+
+	allProjects.style.display = "none";
+	project1.style.display = "none";
+	project2.style.display = "none";
+	project3.style.display = "none";
+	project4.style.display = "none";
+	project5.style.display = "block";
+	projectListButton.style.display = "block";
 }
 
 function showHome(){
@@ -102,14 +199,19 @@ function showAboutMe(){
 }
 
 function showProjects(){
-		let bodyArea = document.querySelector('.bodyArea');
+	let bodyArea = document.querySelector('.bodyArea');
 	
 	let aboutMePage = document.querySelector('.aboutMePage');
 	let projectsPage = document.querySelector('.projectsPage');
+	let projectListButton = document.querySelector('.projectList');
+	//let allProjects = document.querySelector('.allProjects');
 	console.log(projectsPage);
 
 	bodyArea.style.display = "none";
 	aboutMePage.style.display = "none";
 	projectsPage.style.display = "block";
+	projectListButton.style.display = "none";
+	displayProjectsList();
+	//allProjects.style.display = "block";
 
 }
